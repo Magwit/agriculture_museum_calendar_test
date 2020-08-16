d11eba867dcddbe619c8061b118e7f37c7a0ab3d
Magwit <magnushakon@hotmail.com>
2020-08-16 20:45:32 +0200

    build rudimentary given -  when  - then,

--- cypress/integration/event_calendar.spec.js
+++ cypress/integration/event_calendar.spec.js
@@ -1,27 +1,19 @@
-/// <reference types="Cypress" />
+diment /// <reference types="Cypress" />
 
 describe("Event calendar", () => {
-    // GIVEN The user is at the National Museum of Agriculture's (NZM) events calendar
-    it("successfully loads", () => {
-        cy.visit("https://www.nzm.cz/kalendar-akci")
-    })
-    // WHEN The user chooses Prague as location, August 1, 2020 as start date and August 31, 2020 as end date
-    it("Fills in date range and location", () => {
-        cy.get('.select2-selection__arrow').click()
-        cy.get('.select2-search__field').type('Praha{enter}')
+            // GIVEN The user is at the National Museum of Agriculture's (NZM) events calendar
+            it("successfully loads", () => {
+                cy.visit("https://www.nzm.cz/kalendar-akci")
+            })
+            // WHEN The user chooses Prague as location, August 1, 2020 as start date and August 31, 2020 as end date
+            it("Fills in date range and location", () => {
+                    cy.get('.select2-selection__arrow').click()
+                    cy.get('.select2-search__field').type('Praha{enter}')
 
-        cy.get('#DatumOD').type("01.08.2020")
-        cy.get('#DatumDO').type("31.08.2020")
-
-        cy.get('.btn > .fa').click()
-
-    })
+                    cy.get('#DatumOD').type("01.08.2020")
+                    cy.get('#DatumDO').type("        // THEN The user can see NZM events that take place in Prague during August 2020
+                        cy.get('.btn > .fa').click() cy.url().should('include', 'coHledate')
+                    })
 
 
-})
-
-
-
-
-
-// THEN The user can see NZM events that take place in Prague during August 2020
+            })
