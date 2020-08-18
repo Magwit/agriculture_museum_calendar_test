4ec24a6c810234cbf838e012adabf14230d2748d
Magwit <magnushakon@hotmail.com>
2020-08-18 20:09:53 +0200

    create basic structure with visual test

--- cypress/integration/event_calendar.spec.js
+++ cypress/integration/event_calendar.spec.js
@@ -1,36 +1,35 @@
 /// <reference types="Cypress" />
-require('dotenv').config();
 
 describe("Event calendar", () => {
-    // beforeEach(() => {
-    //     cy.eyesOpen({
-    //         appname: 'nzm, Czech Republic',
-    //         testName: 'Looking at the event calendar',
-    //         browser: {
-    //             width: 800,
-    //             height: 600
-    //         }
-    //     })
-    // })
-    // GIVEN The user is at the National Museum of Agriculture's (NZM) events calendar
-    it("successfully loads", () => {
-        cy.visit("https://www.nzm.cz/kalendar-akci")
+    beforeEach(() => {
+        cy.eyesOpen({
+            // appname: 'nzm, Czech Republic',
+            testName: 'Looking at the event calendar',
+            browser: {
+                width: 1000,
+                height: 660,
+            }
+        })
     })
+    // GIVEN The user is at the National Museum of Agriculture's (NZM) events calendar
     // WHEN The user chooses Prague as location, August 1, 2020 as start date and August 31, 2020 as end date
     it("Fills in date range and location", () => {
+        cy.visit("https://www.nzm.cz/kalendar-akci")
         cy.get('.select2-selection__arrow').click()
         cy.get('.select2-search__field').type('Praha{enter}')
-        // more
-
+        cy.get('#DatumOD').type('01.08.2020')
+        cy.get('#DatumDO').type('31.08.2020')
+        cy.get('.btn > .fa').click()
 
         // THEN The user can see NZM events that take place in Prague during August 2020
 
-        cy.get('#DatumDO').type("")
-
         cy.url().should('include', 'coHledate')
+        cy.eyesCheckWindow()
 
-        cy.get('.btn > .fa').click()
     })
 
+    afterEach(() => {
+        cy.eyesClose();
+    });
 
 })
