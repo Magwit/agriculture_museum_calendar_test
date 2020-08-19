1399dc3845bd1204c7326e12ff13f4f74de12734
Magwit <magnushakon@hotmail.com>
2020-08-19 08:06:19 +0200

    use baseUrl

--- cypress/integration/event_calendar.spec.js
+++ cypress/integration/event_calendar.spec.js
@@ -14,7 +14,8 @@ describe("Event calendar", () => {
     // GIVEN The user is at the National Museum of Agriculture's (NZM) events calendar
     // WHEN The user chooses Prague as location, August 1, 2020 as start date and August 31, 2020 as end date
     it("Fills in date range and location", () => {
-        cy.visit("https://www.nzm.cz/kalendar-akci")
+        Cypress.config()
+        cy.visit("/kalendar-akci")
         cy.get('.select2-selection__arrow').click()
         cy.get('.select2-search__field').type('Praha{enter}')
         cy.get('#DatumOD').type('01.08.2020')
